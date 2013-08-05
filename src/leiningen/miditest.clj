269d35283562eab182a8f804387900a283e97232
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-08-06 01:17:46 +0200

    Make some noise.

--- src/leiningen/miditest.clj
+++ src/leiningen/miditest.clj
@@ -1,6 +1,36 @@
-(ns leiningen.miditest)
+(ns leiningen.miditest
+  (:import (javax.sound.midi MidiSystem Synthesizer)))
+
+(def default-note 60)
+(def default-velocity 128)
+(def default-duration 1000)
+
+(defn play-note [channel note-map]
+  (let [{:keys [note velocity duration]
+         :or {note default-note
+              velocity default-velocity
+              duration default-duration}} note-map]
+    (. channel noteOn note velocity)
+    (Thread/sleep duration)
+    (. channel noteOff note)))
+
+(defn find-instrument
+  [synth instrument-name]
+  (first (filter #(= instrument-name (.getName %))
+                 (.getAvailableInstruments synth))))
+
+(defn play-instrument
+  [instrument-name]
+  (with-open [synth (doto (MidiSystem/getSynthesizer) .open)]
+    (let [channel (aget (.getChannels synth) 0)
+          instrument (find-instrument synth instrument-name)]
+      (when instrument
+        (.loadInstrument synth instrument)
+        (.programChange channel (.. instrument getPatch getProgram))
+        (play-note channel {:duration 1000})
+        (Thread/sleep 1000)))))
 
 (defn miditest
   "I don't do a lot."
   [project & args]
-  (println "Hi!"))
+  (play-instrument "French Horn"))
