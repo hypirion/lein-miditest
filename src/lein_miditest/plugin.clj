762285308d0adf64916fbe78f1eebdcb1077a99c
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-08-06 01:47:18 +0200

    Oh my, this is the best.

--- src/lein_miditest/plugin.clj
+++ src/lein_miditest/plugin.clj
@@ -1,8 +1,19 @@
 (ns lein-miditest.plugin
-  (:require [leiningen.miditest :as midi]))
+  (:require [leiningen.miditest :as midi]
+            [leiningen.test]))
+
+(defn error-sound []
+  (midi/play-instrument "French Horn"))
+
+(defn play-after [song]
+  (fn [f]
+    (fn [& args]
+      (apply f args)
+      (song))))
 
 (defn hooks
   []
-  (println "Oh, I'm hooked! I'm hooked! Wee :D"))
+  (println "Oh, I'm hooked! I'm hooked! Wee :D")
+  (alter-var-root #'leiningen.test/test (play-after error-sound)))
 
 (alter-var-root #'hooks memoize)
