b9575be224312cbde25fc0fb3d0a54b513304acb
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-08-07 00:33:47 +0200

    Don't play tunes recursively.

--- src/lein_miditest/plugin.clj
+++ src/lein_miditest/plugin.clj
@@ -2,18 +2,21 @@
   (:require [leiningen.miditest :as midi]
             [leiningen.test]))
 
+(def ^:private ^:dynamic *recursive-test* false)
+
 (defn error-sound []
   (midi/play-instrument "French Horn"))
 
 (defn play-after [song]
   (fn [f]
     (fn [& args]
-      (apply f args)
-      (song))))
+      (binding [*recursive-test* true]
+        (apply f args))
+      (when-not *recursive-test*
+        (song)))))
 
 (defn hooks
   []
-  (println "Oh, I'm hooked! I'm hooked! Wee :D")
   (alter-var-root #'leiningen.test/test (play-after error-sound)))
 
 (alter-var-root #'hooks memoize)
