9f22d909fcae54d3acb34d93cc874580e1b989d9
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-08-07 02:47:43 +0200

    Rebind *exit-after-tests* to avoid early shutdown.

--- src/lein_miditest/plugin.clj
+++ src/lein_miditest/plugin.clj
@@ -13,18 +13,21 @@
 (defn play-after [ok failure]
   (fn [f]
     (fn [& args]
-      (try
-        (binding [*recursive-test* true]
-          (apply f args)
-          (ok))
-        (catch Exception e
-          (println "hullo")
-          (when-not *recursive-test*
-            (failure))
-          (throw e))))))
+      (let [exit-after-tests leiningen.test/*exit-after-tests*]
+        (try
+          (binding [*recursive-test* true
+                    leiningen.test/*exit-after-tests* false]
+            (apply f args)
+            (ok))
+          (catch clojure.lang.ExceptionInfo e
+            (println "we're getting here.")
+            (when-not *recursive-test*
+              (failure))
+            (throw e)))))))
 
 (defn hooks
   []
-  (alter-var-root #'leiningen.test/test (play-after ok-sound failure-sound)))
+  (alter-var-root #'leiningen.test/test
+                  (play-after ok-sound failure-sound)))
 
 (alter-var-root #'hooks memoize)
